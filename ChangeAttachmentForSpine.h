70edfb86580ba586e3dc2044f0144aa1b0130c92
kokokele <zp1030@gmail.com>
2015-05-28 14:28:01 +0800

    Create ChangeAttachmentForSpine.h
    add .h

---
+++ ChangeAttachmentForSpine.h
@@ -0,0 +1,15 @@
+
+#ifndef ANTZ__
+#define ANTZ__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "tolua++.h"
+#ifdef __cplusplus
+}
+#endif
+
+TOLUA_API int register_extendsToLua_module(lua_State* L);
+
+#endif // #ifndef COCOS_SCRIPTING_LUA_BINDINGS_LUA_COCOS2DX_SPINE_MANUAL_H
